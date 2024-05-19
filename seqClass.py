7a02410275ee2acda3ab59120b6f3877469f9c29
MariaValdez05 <maho-0590@hotmail.com>
2024-05-19 17:36:37 -0400

    Update seqClass.py

--- seqClass.py
+++ seqClass.py
@@ -1,6 +1,5 @@
 !/usr/bin/env python
 
-# First, we import some useful code 
 import sys, re
 from argparse import ArgumentParser
 
@@ -14,7 +13,6 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-# Fix the seqClass.py script so that it is able to classify correctly any RNA or DNA sequence.
 args.seq = args.seq.upper()                 # Note we just added this line
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):            # Find a T means we have DNA
